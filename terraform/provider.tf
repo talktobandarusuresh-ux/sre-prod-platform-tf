688833f49bcab9f6afd1d48f3a567dbea8639fe8
Suresh Bandaru <bandarucloudmasters@gmail.com>
2026-03-09 14:07:34 +0530

    aws prod setup

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,9 +10,9 @@ provider "helm" {
   }
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+# }
 
