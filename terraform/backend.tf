df76d4019c57539a6240a80d069fa9fff5b1e3fe
Suresh Bandaru <bandarucloudmasters@gmail.com>
2026-03-09 12:41:39 +0530

    aws prod setup

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,15 +1,24 @@
+# terraform {
+
+#   backend "s3" {
+
+#     bucket = "sre-terraform-state-bucket"
+
+#     key = "sre-platform/terraform.tfstate"
+
+#     region = "us-east-1"
+
+#     dynamodb_table = "terraform-lock"
+
+#     encrypt = true
+#   }
+# }
+
 terraform {
-
   backend "s3" {
-
-    bucket = "sre-terraform-state-bucket"
-
-    key = "sre-platform/terraform.tfstate"
-
-    region = "us-east-1"
-
-    dynamodb_table = "terraform-lock"
-
+    bucket  = "sre-terraform-state-bucket"
+    key     = "sre-platform/terraform.tfstate"
+    region  = "us-east-1"
     encrypt = true
   }
 }
