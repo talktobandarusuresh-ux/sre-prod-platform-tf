7f2f6e34a927f2d80df5b7ec29944e92744aef92
Suresh Bandaru <bandarucloudmasters@gmail.com>
2026-03-09 13:44:16 +0530

    aws prod setup

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,28 +1,19 @@
 module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
-  source = "terraform-aws-modules/eks/aws"
-
-  cluster_name = var.cluster_name
-
+  cluster_name    = var.cluster_name
   cluster_version = "1.29"
 
-  vpc_id = module.vpc.vpc_id
-
+  vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
-
-    workers = {
-
-      instance_types = ["t3.large"]
-
-      desired_size = 3
-
-      min_size = 2
-
-      max_size = 4
+    default = {
+      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
     }
   }
-
-  tags = local.tags
 }
